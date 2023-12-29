d657491b407e8b3c7aadde35754a28d7199a823c
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2023-12-28 21:09:22 -0500

    Show title and exits

---
+++ Git-Watcher.ps1
@@ -0,0 +1,7 @@
+Write-Host -ForegroundColor DarkGreen '  ____ _ _    __        __    _       _               '
+Write-Host -ForegroundColor DarkGreen ' / ___(_) |_  \ \      / /_ _| |_ ___| |__   ___ _ __ '
+Write-Host -ForegroundColor DarkGreen '| |  _| | __|  \ \ /\ / / _` | __/ __| ''_ \ / _ \ ''__|'
+Write-Host -ForegroundColor DarkGreen '| |_| | | |_    \ V  V / (_| | || (__| | | |  __/ |   '
+Write-Host -ForegroundColor DarkGreen ' \____|_|\__|    \_/\_/ \__,_|\__\___|_| |_|\___|_|   '
+Write-Host
+Write-Host
